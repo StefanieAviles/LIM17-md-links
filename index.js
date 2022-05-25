bb02a129d26cca3cf8625ef8f68cc60d40f6179c
StefanieAviles <stefanie42019@gmail.com>
2022-05-24 23:20:21 -0500

    Validacion de links con axios version 1(falta manejar extraer informacion de error

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 const chalk = require ('chalk');
 const {
-  readPath, validateAbsolutePath, validatePath
+  readPath, validateAbsolutePath, validatePath, makeArrayObject, validateLinks
 } = require('./cli.js');
+const axios = require('axios');
 
 let UserPath = process.argv[2];
 let UserOptions = process.argv[3];
-const mdLinks = (path, /* options */) => /*new Promise((resolve, reject) =>*/ {
+async function mdLinks(path, options) /*new Promise((resolve, reject) =>*/ {
   let resultsLinks= [];
   if (!path){
     console.log(chalk.red('*** DEBES INGRESAR UNA RUTA ***'));
@@ -14,10 +15,28 @@ const mdLinks = (path, /* options */) => /*new Promise((resolve, reject) =>*/ {
   else {
     let absoluteUserPath = validateAbsolutePath(path);
     if(validatePath(absoluteUserPath)){
-      console.log('*** La ruta es valida ***');
+      // console.log('*** La ruta es valida ***');
       resultsLinks = readPath(absoluteUserPath);
       if(resultsLinks.length > 0){        
-        console.log(resultsLinks);      
+        // console.log(resultsLinks); 
+        let arrayObjects = makeArrayObject(resultsLinks);
+        if(!options){
+          console.log(arrayObjects); 
+        }
+        else if(options === '--validate'){
+          let newArr=[];
+          for(i=0; i<arrayObjects.length; i++){          
+            newArr.push(await validateLinks(arrayObjects[i]));
+          }
+          console.log(newArr);
+        }
+        else {
+          console.log(chalk.yellow('Las opciones correctas de validacion son:')); 
+          console.log(chalk.yellow('--validate '));
+          console.log(chalk.yellow('--stats ')); 
+          console.log(chalk.yellow('--stats --validate '));    
+        }
+        
       }
       else{
         console.log(chalk.red('--- No existen links en la ruta especificada ---'));  
@@ -28,7 +47,7 @@ const mdLinks = (path, /* options */) => /*new Promise((resolve, reject) =>*/ {
   }
 }
 
-mdLinks(UserPath);
+mdLinks(UserPath, UserOptions);
 
 module.exports = () => {
   mdLinks
